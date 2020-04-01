963ab17a8dbbf1769b09ad239332b13a0e4d371f
Alex Careja <alexandrucareja@yahoo.com>
2020-04-01 06:49:02 -0400

    Add files via upload

--- Lab 07/Careja_Alexandru_Cristian_324_CDb.c
+++ Lab 07/Careja_Alexandru_Cristian_324_CDb.c
@@ -73,9 +73,13 @@ void print_queue(Queue *);
 
 char get_char(int );
 
+void DFS();
+
+void explorare(int , int *, int *, int *, int *, int *, Queue **);
+
 int main() {
-	
 	BFS();
+	DFS();
 	return 0;
 }
 
@@ -176,4 +180,73 @@ char get_char(int n) {
 		x++;
 	}
 	return x;
+}
+
+void DFS() {
+	/*
+	Codific A = 0, B = 1, C = 2 .. J = 9, si pentru afiasre folosesc o functie de parsare a numarului in litera corespunzatoare
+	*/
+	Queue **vector_adiacenta = (Queue **) malloc(10 * sizeof(Queue *));
+	int i, dimensiune;
+	dimensiune = 10;
+	for(i = 0; i < 10; i++) {
+		vector_adiacenta[i] = createQueue();
+	}
+	// adaugarea arcelor din graful orientat
+	enqueue(vector_adiacenta[0], 1); // A->B
+	enqueue(vector_adiacenta[0], 2); // A->C
+	enqueue(vector_adiacenta[0], 7); // A->H
+	enqueue(vector_adiacenta[1], 3); // B->D
+	enqueue(vector_adiacenta[1], 4); // B->E
+	enqueue(vector_adiacenta[1], 7); // B->H
+	enqueue(vector_adiacenta[1], 8); // B->I
+	enqueue(vector_adiacenta[2], 3); // C->D
+	enqueue(vector_adiacenta[3], 4); // D->E
+	enqueue(vector_adiacenta[3], 9); // D->J
+	enqueue(vector_adiacenta[4], 5); // E->F
+	enqueue(vector_adiacenta[4], 6); // E->G
+	enqueue(vector_adiacenta[5], 6); // F->G
+	enqueue(vector_adiacenta[7], 8); // H->I
+	enqueue(vector_adiacenta[8], 0); // I->A
+
+	printf("BFS:\n\n");
+	// alocare dinamica vectori ce urmeaza a fi folsiti
+	int *d = (int *) malloc(dimensiune * sizeof(int)); // momentul descoperirii
+	int *f = (int *) malloc(dimensiune * sizeof(int)); // momentul terminarii explorarii din nodul curent
+	int *p = (int *) malloc(dimensiune * sizeof(int)); // parinti
+	int *c = (int *) malloc(dimensiune * sizeof(int)); // culori: a = alb, g = gri, n = negru
+	int current, succesor;
+	for(i = 0; i < dimensiune; i++) {
+		c[i] = 'a'; // initial toate nodurile sunt albe
+		p[i] = -1; // nimeni nu are niciun parinte setat
+	}
+	int timp = 0; // variabila pentru moment
+	for(i = 0; i < dimensiune; i++) { // incercam sa parcurgem DFS din orice surse posibile
+		if (c[i] == 'a') { // daca nodul i nu este explorat, atunci inseaman ca este sursa si il explorez
+			p[i] = -1; // nodul sursa nu are parinte
+			explorare(i, &timp, d, f, p, c, vector_adiacenta);
+		}
+	}
+}
+
+void explorare(int current, int *timp, int *d, int *f, int *p, int *c, Queue **vector_adiacenta) {
+	int succesor;
+	char litera = get_char(current); // litera nodului curent
+	*timp = *timp + 1; // incrementare moment
+	d[current] = *timp; 
+	c[current] = 'g'; // nod in curs de vizitare => gri
+	printf("%d: c(%c) = gri, d(%c) = %d, p(%c) = %c\n", *timp, litera, litera, *timp, litera, get_char(p[current]));
+	Queue *q = vector_adiacenta[current]; // lista de adiacenta a nodului curent
+	while (!isQueueEmpty(q)) { // pentru fiecare vecin
+		succesor = front(q);
+		dequeue(q);
+		if (c[succesor] == 'a') { // daca nu este vizitat, il vizitez acum
+			p[succesor] = current; // setez parinte
+			explorare(succesor, timp, d, f, p, c, vector_adiacenta);
+		}
+	}
+	c[current] = 'n';
+	*timp = *timp + 1;
+	f[current] = *timp;
+	printf("%d: c(%c) = negru, f(%c) = %d\n", *timp, litera, litera, *timp);
 }
